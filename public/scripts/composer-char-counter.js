f294515e849e0464c962212f61a08ec9d465721a
VictorDQuintero <131626735+VictorDQuintero@users.noreply.github.com>
2024-07-07 21:44:10 -0600

    Add code to count characters in textarea, add a new class when remaining characters are negative, remove when remaining characters are positive, display remaining chars in counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,21 @@
 $(document).ready(function() {
   // --- our code goes here ---
+  // $('#tweet-text').input(function() {
+  //   console.log("input");
+  // });
+  $('#tweet-text').on('input', function() {
+    const charCount = $(this).val().length;
+    const remainingChars = 140 - charCount;
+       
+
+    if(remainingChars < 0 ){
+      $(this).parent().find('output').addClass('counter-negative');
+    } else {
+      $(this).parent().find('output').removeClass('counter-negative');
+    }
+    
+    $(this).parent().find('output').text(remainingChars)
+  });
+
+ 
 });
