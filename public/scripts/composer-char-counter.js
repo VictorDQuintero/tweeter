0c4545ace097323fe4868cfee2d2c91f927e20c0
VictorDQuintero <131626735+VictorDQuintero@users.noreply.github.com>
2024-07-07 21:49:53 -0600

    Add comments

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,20 +1,17 @@
 $(document).ready(function() {
-  // --- our code goes here ---
-  // $('#tweet-text').input(function() {
-  //   console.log("input");
-  // });
+  
   $('#tweet-text').on('input', function() {
-    const charCount = $(this).val().length;
-    const remainingChars = 140 - charCount;
+    const charCount = $(this).val().length; // counts characters in textarea
+    const remainingChars = 140 - charCount; // counts remaining characters
        
 
-    if(remainingChars < 0 ){
+    if(remainingChars < 0 ){ // if negative characters add class to counter, which in css will be turned red
       $(this).parent().find('output').addClass('counter-negative');
     } else {
       $(this).parent().find('output').removeClass('counter-negative');
     }
     
-    $(this).parent().find('output').text(remainingChars)
+    $(this).parent().find('output').text(remainingChars) // displays remainingChars
   });
 
  
