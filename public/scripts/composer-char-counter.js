fee8d2111b0b28ee79dde6a2cc7f21cd322418dd
VictorDQuintero <131626735+VictorDQuintero@users.noreply.github.com>
2024-07-10 20:57:37 -0600

    Assign the jQuery line to output

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,15 +3,15 @@ $(document).ready(function() {
   $('#tweet-text').on('input', function() {
     const charCount = $(this).val().length; // counts characters in textarea
     const remainingChars = 140 - charCount; // counts remaining characters
-       
+    const output = $(this).parent().find('output').text(remainingChars)
 
     if(remainingChars < 0 ){ // if negative characters add class to counter, which in css will be turned red
-      $(this).parent().find('output').addClass('counter-negative');
+      output.addClass('counter-negative');
     } else {
-      $(this).parent().find('output').removeClass('counter-negative');
+      output.removeClass('counter-negative');
     }
     
-    $(this).parent().find('output').text(remainingChars) // displays remainingChars
+    output.text(remainingChars) // displays remainingChars
   });
 
  
